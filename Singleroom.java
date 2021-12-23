8113c9ece9b2aa244a918b0ee2c3206b9b696cfd
Ayush Raj <77911522+sayhi2ayush@users.noreply.github.com>
2021-12-23 12:47:24 +0530

    Add files via upload

---
+++ Singleroom.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+class Singleroom implements Serializable
+{
+    String name;
+    String contact;
+    String gender;   
+    ArrayList<Food> food =new ArrayList<>();
+
+   
+    Singleroom()
+    {
+        this.name="";
+    }
+    Singleroom(String name,String contact,String gender)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+    }
+}
